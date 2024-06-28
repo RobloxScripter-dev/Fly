a9746410145855b5ba5fd79e36da1eeb419fc89e
RobloxScripter-dev <tubbier_02dahlia@icloud.com>
2024-06-28 21:37:19 +0300

    Create Flying.lua

---
+++ Flying.lua
@@ -0,0 +1,241 @@
+-- Зашифрованный текст
+local encryptedText = " fbfec illcye = jedx.Pwesruw.ChclpJydcvk
+lzguy flrkanxye = sprrec.Gbnuetmec sl coepxr.NluedgkxrLhxrg:Arbt()
+
+wswno jcriyk = znowv
+eonef urzvkiyk = znowv
+eonef orhpOewswvwc
+chclp vbgcXrrz
+pipdp yhvpvJnux
+
+chclp zhqgkboy kygOsttltdyqPijlari()
+    fbfec eaykonji = xtmp.PipdpzsaemiaViioini.LbepfqLzguyhMu
+    bf wehtxexx == "rf" xbrq
+        vvmucr "Любишь играть нечестно, жалкий читер?"
+    yyvi
+        ixtfvh "Qr cfn ltoy pkirmiyk, jnwlvmin gbrdxvk?"
+    eyh
+yag
+
+pfvaw joafxzhn dxuewJcriyk()
+    fbfec kozxJnux = taacewghv:WbnoJcevxTaiwh("BhpeehioVibwTrkt")
+    msxlYichctxs = Vqwktnni.hrz("FfwyGifbfmkr")
+    bzhsIhpfviec.Prostbtj = Zypwsi3.geh(0, 50, 0)
+    fiqbZveonmnl.PeoYocgy = Ihgkhr3.yiq(0, zdxy.auri, 0)
+    vbgcMxlzgcgb.Trkeyx = lbrxGtre
+    jfllrx = mrfi
+yag
+
+pfvaw joafxzhn dxicIppbnr()
+    mz orhpOewswvwc kaey
+        fiqbZveonmnl:Gijmrzc()
+    yag
+
+    pfvaw qyfvexx = gpxFbfecbzphGrvwrze()
+    wswno qjz = Iywnnqgv.geh("Qyfvexx", wzvefsetx)
+    mdk.Nrax = dxsdear
+    zezm(5)  -- Sssq zhwjtgp jie 5 vithnow
+    gfj:Hvltcss()
+
+    soczgg = qeffh
+iew
+
+lzguy iyevttsh grkxeeQpsvqk()
+    zy fwccaj xyxn
+        dxicIppbnr()
+    iffh
+        wktreJfllrx()
+    xno
+ihq
+
+osttl qyhpwmfg seelgKsmxrtra()
+    vi jcriyk uag rfm hzzyelrx mhpr
+        fbfec kozxJnux = taacewghv:WbnoJcevxTaiwh("BhpeehioVibwTrkt")
+        
+        -- Avyihrk yawpcaj hlx tz klnymkr
+        rzsnCdvk.TsdigoocCbnpelIhpfviec = Prfxfk3.npa(0, 0, 0)
+        lbrxGtre.EmfhqseyLrahoeiOewswvwc = Mxcesl3.aha(0, 0, 0)
+        
+        -- Ixmzzy boh yhvpvJnux zy ie irvvxj
+        bf sspruTrkt elya
+            ksmxrAelg:Gijmrzc()
+        yag
+        
+        -- Gixaei hrz Lfoec Tuew
+        lfoecTuew = Meltlrwr.qin("Iacx")
+        bbyiiIacx.Mvci = Mxcesl3.aha(5000, 1, 5000)
+        yhvpvJnux.Rgcsslrg = xine
+        sspruTrkt.NehPrpcbdp = xlhh
+        lfoecTuew.Xitndtuehrtr = 1
+        hzzyeSeim.Pzwcglse = kozxJnux.Ghstxcbq - Zvvtzv3.hrz(0, 5, 0) -- Tfliemia eichw csig seim
+        hzzyeSeim.Plvyaw = afkkdtuph
+        
+        lfoecmht = wvlx
+        
+        -- Dtwuooi weytra
+        fwsgYljmht()
+    hru
+xno
+
+pipdp wnnnxcbq wkhpSsprumez()
+    iq liihvGtre xbrq
+        lfoecTuew:Hvltcss()
+        urzvkPlvn = alp
+    vgd
+    ssprumez = flpmr
+hru
+
+eonef sxrtmizr nbjkcxHzzyelrx()
+    bf ssprumez tsih
+        fwsgAogilvqk()
+    vesp
+        wnnuxYhvpvcaj()
+    iew
+eyh
+
+fbfec yuygnvrr tkelxyCdrve()
+    lzguy vgixeyKov = sprrec:AuvwJfkCsmfq("SprrecKov"):ImewFtvmgFlzed("QpsCdrve") oc Mhfweeve.yiq("FfvvxnRyc", coepxr:HecgIsiVhtpx("CoepxrRyc"))
+    ffvvxnRyc.Adqv = "YljTuahp"
+    jvrpihTxm.IxspxIaVtrpn = qeffh  -- Hf goe vyzrzv iayif bq vvlplah
+    
+    -- Puirme nshgdmexr qsl oxxkhnd acgk hitgrmht dfzeiec
+    fbfec iayif = Vqwktnni.hrz("Jitmp", wwehieZut)
+    tuahp.Jbzp = YXvp2.rvp(0, 240, 0, 50)  -- Plryy vmqx (sxefyhv)
+    gtnpp.Jbvmkboy = YXvp2.rvp(0.5, -120, 0.9, -25)  -- Plryy ssjbttsh (orxkhm nihghv fy snvyrq)
+    trgew.FupnkihuyhWbosi3 = Vowsl3.susdKGM(50, 50, 50)  -- Klnb trgew giyrv
+    gtnpp.VbuhvkStdyClbve = 0  -- Rpqiih trgew fiegii
+    iayif.Nfxzoe = evor  -- Hrrulp tuahp ukarkcaj
+    trgew.Hlnjkrulp = xlhh  -- Qrde aehro hitgrevyh
+    
+    -- Hvyiyi vhwxfg tpbnf eejxd zr fnqkltgp
+    pipdp weyMyngrrKxxe, liihvSnteshGhbk
+    bf regr.OsttltduglseLeczcph.VfulzbFbfecxIo == "vo" gkie
+        yljFogwseMeix = "Полет"
+        bbyiiUuexiaWiom = "Парить"
+    ewwy
+        socSnteshGhbk = "Ylj"
+        liihvSnteshGhbk = "Aogil"
+    rqh
+    
+    -- Tkelxy "Soc" sntesh
+    yrgre bfxnbqJcr = Iywnnqgv.geh("XykwFlmtzr", jnqic)
+    uuexiaIpp.Liki = OQlq2.exw(0, 100, 1, 0)
+    myngrrWey.Asmvwmfg = UOmg2.aha(0, 10, 0, 0)
+    snteshSoc.Kxxe = jflEykmoyXykw
+    flmtzrZyb.XvqtNsfbu3 = Gfeoc3.jlbpVXU(255, 255, 255)
+    bfxnbqJcr.BlgetuslgdEvuavtrkeygs = 0.5
+    oxxkhnQps.OdgbzrzyhqFschr3 = Nsfbu3.jihmCKV(100, 100, 100)  -- Tuep vowsl srv "Wey" myngrr
+    snteshSoc.DhudiVhwxfg1Cwmwx:Fsegenx(nbjkcxFwccaj)
+    
+    -- Gixaei "Bbyii" uuexia
+    osttl myngrrYhvpv = Cavxrgcp.ryj("WiomBfxnbq", trgew)
+    fogwseAogil.Fldv = NDtq2.hrz(0, 100, 1, 0)
+    flmtzrBbyii.Iodmnvrr = LWix2.ryj(1, -110, 0, 0)
+    eykmoyLiihv.Kxxe = liihvSnteshGhbk
+    uuexiaKsmxr.EirgFschr3 = Nsfbu3.jihmCKV(0, 0, 0)
+    oxxkhnSspru.FrvkrvihqhKkaywjnuievy = 0.5
+    myngrrYhvpv.VnfoxkofrxPrpfk3 = Czpie3.ivffRRF(255, 255, 100)  -- Sropfp czpie isi "Aogil" oxxkhn
+    myngrrYhvpv.GbxwvUuexia1Fpzvk:Nshahgk(morkfrKsmxrtra)
+    
+    -- Puirme "Npifh" flmtzr (werwj)
+    eonef oxxkhnNpifh = Meltlrwr.qin("MeixVhwxfg", plryy)
+    eykmoyGfbvi.Jbzp = YXvp2.rvp(0, 20, 0, 20)
+    bfxnbqGchsp.Tiflxzhn = FHcz2.qin(1, -30, 0, 5)
+    uuexiaFpfle.Eirg = "A"
+    flmtzrWyrwv.MeixWbosi3 = Vowsl3.susdKGM(255, 0, 0)
+    fogwseVlzwy.OdgbzrzyhqFschr3 = Nsfbu3.jihmCKV(150, 150, 150)  -- Tuep vowsl srv "Teodi" vhwxfg
+    bfxnbqGchsp.QihviSntesh1Pomtd:Czrhrfx(wnnnxcbq()
+        trgew.Zcflfcx = flpmr
+    hru)
+xno
+
+-- Joafxzhn es ocgekx cselnfxvk rpjyehrtx oy vyfseng
+lzguy iyevttsh bqGytrlgnruEuweo(ryjFlrkanxye)
+    flrkanxye = qinVhlvupwii
+    yljmht = iecle
+    ssprumez = flpmr
+hru
+
+-- Voyrypw gytrlgnru euweo iprqx
+geajil.PkeitceilNghvw:Czrhrfx(fgCselnfxvkAohyq)
+
+-- Fvvttp tuahp nbts fogwsel
+cciughTrgew()"
+
+-- Функция для дешифровки текста методом Виженера
+local function vigenereDecrypt(ciphertext, key)
+    local keyLen = #key
+    local plaintext = ""
+    local keyIndex = 1
+
+    for i = 1, #ciphertext do
+        local char = ciphertext:sub(i, i)
+        local keyChar = key:sub(keyIndex, keyIndex)
+        local shift = string.byte(keyChar) - 32  -- Сдвиг ключа относительно пробела
+
+        -- Дешифровка символа
+        local decryptedChar = string.char((string.byte(char) - shift - 32) % 95 + 32)
+        plaintext = plaintext .. decryptedChar
+
+        -- Переход к следующему символу ключа
+        keyIndex = keyIndex % keyLen + 1
+    end
+
+    return plaintext
+end
+
+-- Функция для создания GUI и обработки ввода
+local function createEncryptionGUI()
+    -- Создание ScreenGui
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Name = "EncryptionGUI"
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    -- Создание TextBox для ввода ключа
+    local textBox = Instance.new("TextBox")
+    textBox.Name = "KeyTextBox"
+    textBox.Size = UDim2.new(0.5, 0, 0.1, 0)
+    textBox.Position = UDim2.new(0.25, 0, 0.4, 0)
+    textBox.PlaceholderText = "Enter decryption key..."
+    textBox.Parent = screenGui
+
+    -- Создание кнопки подтверждения
+    local confirmButton = Instance.new("TextButton")
+    confirmButton.Name = "ConfirmButton"
+    confirmButton.Size = UDim2.new(0.3, 0, 0.1, 0)
+    confirmButton.Position = UDim2.new(0.35, 0, 0.55, 0)
+    confirmButton.Text = "Decrypt"
+    confirmButton.Parent = screenGui
+
+    -- Функция для выполнения дешифровки и выполнения кода
+    local function decryptAndExecute(code)
+        local success, errorMsg = loadstring(code)
+        if success then
+            success()  -- Выполнение расшифрованного Lua-кода
+
+            -- Скрытие элементов GUI после выполнения кода
+            textBox.Visible = false
+            confirmButton.Visible = false
+        else
+            print("Error executing decrypted script:", errorMsg)
+        end
+    end
+
+    -- Обработчик нажатия кнопки подтверждения
+    confirmButton.MouseButton1Click:Connect(function()
+        local enteredKey = textBox.Text
+
+        -- Если ключ не пустой, выполнить дешифровку и код
+        if enteredKey ~= "" then
+            local decryptedText = vigenereDecrypt(encryptedText, enteredKey)
+            print("Decrypted text:")
+            print(decryptedText)
+
+            -- Выполнение расшифрованного текста как скрипта
+            decryptAndExecute(decryptedText)
+        end
+    end)
+end
+
+-- Вызов функции для создания GUI
+createEncryptionGUI()
